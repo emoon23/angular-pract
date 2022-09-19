4a208130e721ecbf6b3fb8f0cf18b189f2f33f2d
emoon23 <emoon9323@gmail.com>
2022-09-18 23:51:15 -0500

    implemented routerlink and product details.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { Product, products } from "../products";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
-  constructor() { }
+  //Inject ActivatedRoute into the Constructor() by adding private route:
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  //this method, extracts the productId from the route parameters and find the corresponding product in the products array
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get("productId"));
+
+    //Find the product that correspond with the id provided in route
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
-
 }
