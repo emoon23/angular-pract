4c2729979a6c72748487da155b094416462f0222
Edward Moon <e.moon@vertexwireless.com>
2022-09-19 12:17:47 -0500

    Added User Input

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,7 +11,21 @@ export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
 
-  constructor( private cartService: CartService) { }
+  //Gather user's name & address using the FormBuilder.group() method
+  //This methos sets the checkoutForm property to a form model containing name & address fields
+  checkoutForm = this.formBuilder.group({ name: '', address: ''});
+
+  //Injecting CartService & FormBuilder in the CartComponent constructor()
+  constructor( private cartService: CartService, private formBuilder: FormBuilder) { }
+
+//onSubmit() method process the form. Allows users to submit their name & address
+//This method uses clearCart() method of the CartService to reset the form & clear the cart.
+  onSubmit(): void{
+      //Process checkout data here
+      this.items = this.cartService.clearCart();
+      console.warn('Your order has been submitted', this.checkoutForm.value);
+      this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
   }
