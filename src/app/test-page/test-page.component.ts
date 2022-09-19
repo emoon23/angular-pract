73d3acbf4a2fcc54bcf252a7a0a0ee023a677b95
emoon23 <emoon9323@gmail.com>
2022-09-18 23:17:22 -0500

    Creation of Practice Angular

---
+++ src/app/test-page/test-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+import { cars } from "../tester";
+
+@Component({
+  selector: "app-test-page",
+  templateUrl: "./test-page.component.html",
+  styleUrls: ["./test-page.component.css"],
+})
+export class ProductListComponent {
+  cars = cars;
+
+  share() {
+    window.alert("This car has been shared!");
+  }
+}
---
+++ src/app/tester.ts
@@ -0,0 +1,21 @@
+export interface Tester{
+    id: number;
+    name: string;
+    profession: string,
+    hobby: string;
+}
+
+export const cars = [
+    {
+        id: 1,
+        name: 'Vantage V8 2019',
+        profession: 'Senior Software Developer',
+        hobby: 'Jiu-Jitsu'
+    },
+    {
+        id: 2,
+        name: 'F12 Berlinetta',
+        profession: 'CEO',
+        hobby: 'Golf'
+    }
+];
